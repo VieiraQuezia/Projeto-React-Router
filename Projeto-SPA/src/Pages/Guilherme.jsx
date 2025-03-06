be021438aa0e17bd1c9a6acd60ea4ec3b4536254
Guih <ggsantana18@gmail.com>
2025-03-06 08:28:49 -0300

    base paginas

--- Projeto-SPA/src/Pages/Guilherme.jsx
+++ Projeto-SPA/src/Pages/Guilherme.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import { useState } from "react"
+
 import Guilherme from "/guilherme.png"
 import './base.css'
 
 function guilherme() {
+const [isLogado, setIsLogado] = useState(false);
+
+
   return (
     <>
     <article className="container">
-        
+
       <nav className="header">
         <img
           className="image"
@@ -20,14 +24,18 @@ function guilherme() {
 
           <nav className="seguidores">
               <span>346 Seguidores</span>
-              
               <span>80 Seguindo</span>
           </nav>
+    
+    <button onClick={ () => setIsLogado(!isLogado)}  className={isLogado ? 'seguindo' : 'seguir'} >  {isLogado ? "Seguindo" : "Seguir "} </button> 
+    
 
         </nav>
       </nav>
 
     </article>
+    <h2 className="login">{isLogado ? "Obrigado por me Seguir" : ""}</h2> 
+
     </>
 
   );
