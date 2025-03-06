9c2d8120b5b8b37da75eb9ddb284cdb402b675c1
Hayeska <loredanehayeska@gmail.com>
2025-03-06 08:38:10 -0300

    alt

--- Projeto-SPA/src/Pages/Hayeska.jsx
+++ Projeto-SPA/src/Pages/Hayeska.jsx
@@ -1,24 +1,43 @@
-import Hayeska from "/hayeska.png";
-import React from "react";
-import "./base.css";
+import { useState } from "react"
+
+import Hayeska from "/hayeska.png"
+import './base.css'
 
 function hayeska() {
+const [isLogado, setIsLogado] = useState(false);
+
+
   return (
-    <div className="container">
-      <div className="header">
-        <img className="image" src={Hayeska} />
-        <div className="info">
+    <>
+    <article className="container">
+
+      <nav className="header">
+        <img
+          className="image"
+          src={Hayeska}
+          alt="Profile"
+        />
+        <nav className="info">
+
           <div className="name">hayeska</div>
           <div className="username">@carecaax</div>
-          <div className="seguidores">
 
-          <p>92 Seguidores</p>
-          <p>248 Seguindo</p>
+          <nav className="seguidores">
+              <span>92 Seguidores</span>
+              <span>248 Seguindo</span>
+          </nav>
+    
+    <button onClick={ () => setIsLogado(!isLogado)}  className={isLogado ? 'seguindo' : 'seguir'} >  {isLogado ? "Seguindo" : "Seguir "} </button> 
+    
 
-          </div>
-        </div>
-      </div>
-    </div>
+        </nav>
+      </nav>
+
+    </article>
+    <h2 className="login">{isLogado ? "Obrigado por me Seguir" : ""}</h2> 
+
+    </>
+
   );
 }
 
