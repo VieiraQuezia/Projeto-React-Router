a16c319cc45eb34118e99e2615d6748f0c68add5
Guih <ggsantana18@gmail.com>
2025-03-06 09:54:14 -0300

    final

--- Projeto-SPA/src/Pages/Home.jsx
+++ Projeto-SPA/src/Pages/Home.jsx
@@ -1,38 +1,35 @@
-
-
 import Lado from "../Components/lado.jsx";
-import Header from '../components/header.jsx'
-import Footer from '../components/Footer'
-import NavBar from '../components/Navbar.jsx'
+import Header from "../components/header.jsx";
+import Footer from "../components/Footer";
+import NavBar from "../components/Navbar.jsx";
 import Instagram from "/instagram.png";
 import gmail from "/gmail.png";
-import Quezia from '/quezia.png'
-import Guilherme from '/guilherme.png'
-import Hayeska from '/hayeska.png'
-import Leo from '/leonardo.png'
+import Quezia from "/quezia.png";
+import Guilherme from "/guilherme.png";
+import Hayeska from "/hayeska.png";
+import Leo from "/leonardo.png";
 
 import "../App.css";
-
-
 
 function App() {
   return (
     <>
-<Header/>
-<article className="meio">
+      <Header />
+      <article className="meio">
+        <NavBar />
+        <div className="image">
+          <h2> ⬅ Conheça as nossas páginas </h2>
+          <img src={Guilherme} />
+          <img src={Leo} />
+          <img src={Hayeska} />
+          <img src={Quezia} />
+        </div>
 
-<NavBar/>
-<div className="image">
-<h2> ⬅ Conheça as nossas páginas </h2>
-<img src={Guilherme} />
-<img src={Leo} />
-<img src={Hayeska} />
-<img src={Quezia} />
-</div>
-      <Lado val1="Perseverança" val2="Amizade" val3="Responsabilidade" />
-</article>
+        <Lado val1="Perseverança" val2="Amizade" val3="Responsabilidade" />
+        
+      </article>
 
-        <Footer
+      <Footer
         imgi={Instagram}
         insta="@vieiraquezia08"
         imge={gmail}
@@ -44,4 +41,3 @@ function App() {
 }
 
 export default App;
-
